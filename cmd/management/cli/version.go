da5f0c34bc1e7f6145da535197bff42fdf03b715
Shintarou Okumura <flekystyley@gmail.com>
2022-01-14 16:26:14 +0900

    added management cli

---
+++ cmd/management/cli/version.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Notch-Technologies/wizy/version"
+	"github.com/peterbourgon/ff/ffcli"
+)
+
+var versionCmd = &ffcli.Command{
+	Name: "version",
+	Usage: "version",
+	ShortHelp: "Show Wizy Version",
+	Exec: execVersion,
+}
+
+func execVersion(args []string) error {
+	if len(args) > 0 {
+		log.Fatalf("too many arugments: %q", args)
+	}
+
+	fmt.Println(version.String())
+
+	return nil
+}
