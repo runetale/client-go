2beded8f1e217dc5ed4fd07a89120473ce74962b
Shintarou Okumura <flekystyley@gmail.com>
2022-01-16 18:13:56 +0900

    update management

--- cmd/management/management.go
+++ cmd/management/management.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/Notch-Technologies/wizy/cmd/management/config"
 	"github.com/Notch-Technologies/wizy/paths"
 	"github.com/Notch-Technologies/wizy/types/flagtype"
 	"github.com/Notch-Technologies/wizy/version"
-	"github.com/Notch-Technologies/wizy/utils"
+	"github.com/Notch-Technologies/wizy/store"
 )
 
 var args struct {
@@ -48,90 +44,14 @@ func main() {
 		os.Exit(0)
 	}
 
-	fs, err := loadFileStore()
+	fs, err := store.LoadFileStore(args.storepath)
 	if err != nil {
  	    log.Fatal(err)
 	}
 
 	fmt.Println(fs)
 
-	cfg := loadConfig()
+	cfg := config.LoadConfig(args.configpath, args.domain, args.certfile, args.certkey)
 	fmt.Println(cfg)
 }
 
-func loadConfig() config.Config {
-	b, err := ioutil.ReadFile(args.configpath)
-	switch {
- 	case errors.Is(err, os.ErrNotExist):
- 		return createNewConfig()
- 	case err != nil:
- 	    log.Fatal(err)
- 	    panic("failed to load cofig")
- 	default:
-		var cfg config.Config
- 	    if err := json.Unmarshal(b, &cfg); err != nil {
- 	        log.Fatalf("config: %v", err)
- 	    }
- 	    return cfg
- 	}
-}
-
-func createNewConfig() config.Config {
-	if err := os.MkdirAll(filepath.Dir(args.configpath), 0777); err != nil {
-		log.Fatal(err)
-	}
-
-	cfg := config.Config{
-		StorePath: args.storepath,
-		TLSConfig: config.TLSConfig{
-			Domain: args.domain,
-			Certfile: args.certfile,
-			CertKey: args.certkey,
-		},
-	}
-
-	b, err := json.MarshalIndent(cfg, "", "\t")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = utils.AtomicWriteFile(args.configpath, b, 0600); err != nil {
-		log.Fatal(err)
-	}
-
-	return cfg
-}
-
-// TODO: (shintard) consider whether to manage users in FileStore or in a different structure (json).
-type FileStore struct {
-	path string
-}
-
-func loadFileStore() (*FileStore, error) {
-	b, err := ioutil.ReadFile(args.storepath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			os.MkdirAll(filepath.Dir(args.storepath), 0755)
-			store := FileStore{args.storepath}
-
-			b, err := json.MarshalIndent(store, "", "\t")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err = utils.AtomicWriteFile(args.storepath, b, 0600); err != nil {
-				return nil, err
-			}
-			return &store, nil
-		}
-		return nil, err
-	}
-
-	var fs FileStore
-
-	if err := json.Unmarshal(b, &fs); err != nil {
-		return nil, err
-	}
-
-	return &fs, nil
-}
