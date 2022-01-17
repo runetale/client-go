709628f4f4d9fc596a06004568ebd0229f5154c2
Shintarou Okumura <flekystyley@gmail.com>
2022-01-17 12:13:34 +0900

    added todo

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  # TODO: (shintard) support aarch64 package
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+     in flake-utils.lib.eachSystem systems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        buildDeps = with pkgs; [ git go ];
-        devDeps = with pkgs;
+        buildDeps = with nixpkgs; [ git go ];
+        devDeps = with nixpkgs;
           buildDeps ++ [
             protoc-gen-go
             go_1_17
@@ -19,7 +20,7 @@
             protoc-gen-go-grpc
           ];
       in
-      { devShell = pkgs.mkShell {
+      { devShell = nixpkgs.mkShell {
         buildInputs = devDeps; 
 
         shellHook = ''
