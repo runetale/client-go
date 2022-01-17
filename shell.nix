b0c13e57a9f9716f4d7c0b01930f05bcc40fe823
Shintarou Okumura <flekystyley@gmail.com>
2022-01-17 12:04:29 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,4 +4,10 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     go_1_17 goimports gopls protobuf protoc-gen-go
   ];
+
+  shellHook = ''
+    export GOPATH=$GOPATH
+    PATH=$PATH:$GOPATH/bin
+    export GO111MODULE=on
+  '';
 }
