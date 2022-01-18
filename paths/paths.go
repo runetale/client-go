994dc192aeb6ae350ac4721b0f35a58926bc9383
Shintarou Okumura <flekystyley@gmail.com>
2022-01-19 00:04:53 +0900

    fix paths

--- paths/paths.go
+++ paths/paths.go
@@ -5,19 +5,33 @@ import (
 )
 
 // File that manages the configuration of the wics server.
-func DefaultWicsFile() string {
+func DefaultWicsConfigFile() string {
 	return "/etc/wizy/wics.json"
 }
 
 // State file to wics the state of the Store.
-func DefaultWicsStateFile() string {
+func DefaultAccountStateFile() string {
 	switch runtime.GOOS {
 	case "freebsd", "openbsd":
-		return "/var/db/wizy/wics.state"
+		return "/var/db/wizy/account.state"
 	case "linux":
-		return "/var/lib/wizy/wics.state"
+		return "/var/lib/wizy/account.state"
 	case "darwin":
-		return "/Library/wizy/wics.state"
+		return "/Library/wizy/account.state"
+	default:
+		return ""
+	}
+}
+
+// State file to wics the state of the Store.
+func DefaultServerStateFile() string {
+	switch runtime.GOOS {
+	case "freebsd", "openbsd":
+		return "/var/db/wizy/server.state"
+	case "linux":
+		return "/var/lib/wizy/server.state"
+	case "darwin":
+		return "/Library/wizy/server.state"
 	default:
 		return ""
 	}
