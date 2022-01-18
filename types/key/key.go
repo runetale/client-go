4c49f74c626cceae37f857b058f9d4bc0348c367
Shintarou Okumura <flekystyley@gmail.com>
2022-01-18 18:08:47 +0900

    added key.go

---
+++ types/key/key.go
@@ -0,0 +1,59 @@
+package key
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	"go4.org/mem"
+	"golang.org/x/crypto/curve25519"
+)
+
+type Key [32]byte
+
+func (k Key) Public() Key {
+	var pub [32]byte
+	curve25519.ScalarBaseMult(&pub, (*[32]byte)(&k))
+	return Key(pub)
+}
+
+func ToHex(k []byte, prefix string) []byte {
+	ret := make([]byte, len(prefix)+len(k)*2)
+	copy(ret, prefix)
+	hex.Encode(ret[len(prefix):], k)
+	return ret
+}
+
+func ParseHex(out []byte, in, prefix mem.RO) error {
+	if !mem.HasPrefix(in, prefix) {
+		return fmt.Errorf("key hex string doesn't have expected type prefix %s", prefix.StringCopy())
+	}
+	in = in.SliceFrom(prefix.Len())
+	if want := len(out) * 2; in.Len() != want {
+		return fmt.Errorf("key hex has the wrong size, got %d want %d", in.Len(), want)
+	}
+	for i := range out {
+		a, ok1 := fromHexChar(in.At(i*2 + 0))
+		b, ok2 := fromHexChar(in.At(i*2 + 1))
+		if !ok1 || !ok2 {
+			return errors.New("invalid hex character in key")
+		}
+		out[i] = (a << 4) | b
+	}
+
+	return nil
+}
+
+func fromHexChar(c byte) (byte, bool) {
+	switch {
+	case '0' <= c && c <= '9':
+		return c - '0', true
+	case 'a' <= c && c <= 'f':
+		return c - 'a' + 10, true
+	case 'A' <= c && c <= 'F':
+		return c - 'A' + 10, true
+	}
+
+	return 0, false
+}
+
