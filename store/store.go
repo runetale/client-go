80cc81316ec8e03ef651e11364e48fe0de5138fd
Shintarou Okumura <flekystyley@gmail.com>
2022-01-19 13:02:08 +0900

    update store;

--- store/store.go
+++ store/store.go
@@ -1,51 +1,11 @@
 package store
 
-// Management FileStore
+import "errors"
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
+type StateKey string
 
-	"github.com/Notch-Technologies/wizy/utils"
+var ErrStateNotFound = errors.New("state not found")
+
+const (
+	ServerPrivateKeyStateKey = StateKey("server-private-key")
 )
-
-type FileStoreManager interface {
-}
-
-type FileStore struct {
-	path string
-}
-
-// TODO: (shintard) user store to key store wo wakeru
-func NewFileStore(path string) (*FileStore, error) {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			os.MkdirAll(filepath.Dir(path), 0755)
-			store := FileStore{path}
-
-			b, err := json.MarshalIndent(store, "", "\t")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err = utils.AtomicWriteFile(path, b, 0600); err != nil {
-				return nil, err
-			}
-			return &store, nil
-		}
-		return nil, err
-	}
-
-	var fs FileStore
-
-	if err := json.Unmarshal(b, &fs); err != nil {
-		return nil, err
-	}
-
-	return &fs, nil
-}
-
