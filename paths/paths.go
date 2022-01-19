19275804436b0e4697d8411d60c8e885596995d5
Shintarou Okumura <flekystyley@gmail.com>
2022-01-19 13:02:00 +0900

    update path

--- paths/paths.go
+++ paths/paths.go
@@ -1,15 +1,18 @@
 package paths
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"runtime"
 )
 
-// File that manages the configuration of the wics server.
+// json file to manage the wics server startup config.
 func DefaultWicsConfigFile() string {
 	return "/etc/wizy/wics.json"
 }
 
-// State file to wics the state of the Store.
+// state file to manage the account state.
 func DefaultAccountStateFile() string {
 	switch runtime.GOOS {
 	case "freebsd", "openbsd":
@@ -23,8 +26,8 @@ func DefaultAccountStateFile() string {
 	}
 }
 
-// State file to wics the state of the Store.
-func DefaultServerStateFile() string {
+// state file to manage the secret information of the wics server.
+func DefaultWicsServerStateFile() string {
 	switch runtime.GOOS {
 	case "freebsd", "openbsd":
 		return "/var/db/wizy/server.state"
@@ -36,3 +39,36 @@ func DefaultServerStateFile() string {
 		return ""
 	}
 }
+
+func MkStateDir(dirPath string) error {
+    if err := os.MkdirAll(dirPath, 0700); err != nil {
+        return err
+    }
+
+    return checkStateDirPermission(dirPath)
+}
+
+func checkStateDirPermission(dir string) error {
+	const (
+		perm = 700
+	)
+
+    if filepath.Base(dir) != "wizy" {
+        return nil
+    }
+
+    fi, err := os.Stat(dir)
+    if err != nil {
+        return err
+    }
+
+    if !fi.IsDir() {
+        return fmt.Errorf("expected %q is a directory, but %v", dir, fi.Mode())
+    }
+
+    if fi.Mode().Perm() == perm {
+        return nil
+    }
+    return os.Chmod(dir, perm)
+}
+
