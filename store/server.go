7831d3bf392ec7d5daec9f8652a035c2c4237298
Shintarou Okumura <flekystyley@gmail.com>
2022-01-19 14:04:02 +0900

    update server.go

--- store/server.go
+++ store/server.go
@@ -1,11 +1,45 @@
 package store
 
-import "sync"
+import (
+	"log"
+	"sync"
 
-type ServerManager inteface {
+	"github.com/Notch-Technologies/wizy/types/key"
+)
+
+type ServerManager interface {
+	WritePrivateKey() error
 }
 
 type Server struct {
-	FileStoreManager FileStoreManager
-	mu sync.Mutex
+	storeManager FileStoreManager
+	mu               sync.Mutex
+}
+
+func NewServer(f FileStoreManager) *Server {
+	return &Server{
+		storeManager: f,
+		mu:               sync.Mutex{},
+	}
+}
+
+func (s *Server) WritePrivateKey() error {
+	k, err := key.NewServerPrivateKey()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	ke, err := k.MarshalText()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	if err := s.storeManager.WriteState(ServerPrivateKeyStateKey, ke); err != nil {
+		log.Fatalf("error writing server private key to store: %v", err)
+		return err
+	}
+
+	return nil
 }
