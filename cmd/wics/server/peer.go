0125dd4eaf11ead1afdac700317b44532149865d
Shintarou Okumura <flekystyley@gmail.com>
2022-01-29 23:39:37 +0900

    update serve

---
+++ cmd/wics/server/peer.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"context"
+
+	"github.com/Notch-Technologies/wizy/cmd/wics/proto"
+	"github.com/Notch-Technologies/wizy/cmd/wics/server/redis"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type PeerServiceServer struct {
+	redis *redis.RedisClient
+	proto.UnimplementedPeerServiceServer
+}
+
+func NewPeerServiceServer(r *redis.RedisClient) *PeerServiceServer {
+	return &PeerServiceServer{
+		redis: r,
+	}
+}
+
+func (pss *PeerServiceServer) WSync(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
+	panic("not implement WSync")
+}
--- cmd/wics/server/wics_server.go
+++ cmd/wics/server/wics_server.go
@@ -1,18 +1,12 @@
 package server
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/Notch-Technologies/wizy/cmd/wics/config"
-	"github.com/Notch-Technologies/wizy/cmd/wics/proto"
 	"github.com/Notch-Technologies/wizy/cmd/wics/server/redis"
 	"github.com/Notch-Technologies/wizy/store"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type Server struct {
-	// grpcServer
 	UserServiceServer *UserServiceServer
 	PeerServiceServer *PeerServiceServer
 }
@@ -22,58 +16,4 @@ func NewServer(config *config.Config, account *redis.AccountStore, server *store
 		UserServiceServer: NewUserServiceServer(r, config, account, server),
 		PeerServiceServer: NewPeerServiceServer(r),
 	}, nil
-}
-
-// TODO:(shintard) create a service for each of the gRPC Servers.
-type UserServiceServer struct {
-	redis *redis.RedisClient
-	config       *config.Config
-	accountStore *redis.AccountStore
-	serverStore  *store.ServerStore
-	
-	proto.UnimplementedUserServiceServer
-}
-
-func NewUserServiceServer(
-	r *redis.RedisClient, config *config.Config, account *redis.AccountStore,
-	server *store.ServerStore,
-) *UserServiceServer {
-	return &UserServiceServer{
-		redis: r,
-		config: config,
-		accountStore: account,
-		serverStore: server,
-	}
-}
-
-// UserService
-//
-func (uss *UserServiceServer) Login(ctx context.Context, msg *proto.LoginMessage) (*proto.LoginMessage, error) {
-	a := msg.GetPublicMachineKey()
-	peer, err := uss.accountStore.GetPeer(a)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	fmt.Println(peer)
-
-	return nil, err
-}
-
-// PeerService
-//
-type PeerServiceServer struct {
-	redis *redis.RedisClient
-	proto.UnimplementedPeerServiceServer
-}
-
-func NewPeerServiceServer(r *redis.RedisClient) *PeerServiceServer {
-	return &PeerServiceServer{
-		redis: r,
-	}
-}
-
-func (pss *PeerServiceServer) WSync(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
-	panic("not implement WSync")
 }
