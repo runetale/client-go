961494422bb26eef0d2311dc2bc280c34bf1b567
Shintarou Okumura <flekystyley@gmail.com>
2022-02-01 15:13:15 +0900

    update admin middleware

--- cmd/wics/server/api/middleware.go
+++ cmd/wics/server/api/middleware.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/Notch-Technologies/wizy/client"
 )
 
 func adminMiddleware(next http.Handler) http.Handler {
@@ -12,6 +15,18 @@ func adminMiddleware(next http.Handler) http.Handler {
 	if !strings.HasPrefix(sub, "auth0") {
     	http.Error(w, "invalid value", http.StatusBadRequest)
 	}
+
+	accessToken, err := client.GetAuth0ManagementAccessToken()
+	if err != nil {
+    	http.Error(w, fmt.Sprintf("%s", err.Error()), http.StatusBadRequest)
+	}
+
+	isAdmin, err := client.IsAdmin(sub, accessToken)
+	if err != nil || !isAdmin {
+    	http.Error(w, fmt.Sprintf("%s", err.Error()), http.StatusBadRequest)
+	}
+
+	fmt.Println(isAdmin)
 
     next.ServeHTTP(w, r)
   })
