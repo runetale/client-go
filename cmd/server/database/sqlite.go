235e6d50c71fe79bed95ed97efa75183b2d8ce4f
Shintarou Okumura <flekystyley@gmail.com>
2022-02-05 22:18:45 +0900

    added tx

--- cmd/server/database/sqlite.go
+++ cmd/server/database/sqlite.go
@@ -128,4 +128,53 @@ type Tx struct {
 	tx          *sql.Tx
 }
 
+func (t *Tx) Exec(query string, args ...interface{}) (int64, error) {
+	r, err := t.tx.Exec(query, args...)
+	if err != nil {
+		return 0, err
+	}
+	if err != nil {
+		return 0, fmt.Errorf("%s", err.Error())
+	}
 
+	return r.LastInsertId()
+}
+
+// Multi Select
+func (t *Tx) Query(query string, dest interface{}, args ...interface{}) error {
+	rows, err := t.tx.Query(query, args...)
+	if err != nil {
+		return fmt.Errorf("%s", err.Error())
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&dest)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Single Select
+func (t *Tx) QueryRow(query string, dest interface{}, args ...interface{}) error {
+	row := t.tx.QueryRow(query, args...)
+	err := row.Scan(&dest)
+	if err != nil {
+    	if err == sql.ErrNoRows {
+			return nil
+    	} else {
+			return err
+    	}
+	}
+	return nil
+}
+
+func (t *Tx) Commit() error {
+	return t.tx.Commit()
+}
+
+func (t *Tx) Rollback() error {
+	return t.tx.Rollback()
+}
