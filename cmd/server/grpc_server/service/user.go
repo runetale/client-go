ff128815fcb332e57983681fa6b2184501f468a8
Shintarou Okumura <flekystyley@gmail.com>
2022-02-07 10:31:03 +0900

    refactor

--- cmd/server/grpc_server/service/user.go
+++ cmd/server/grpc_server/service/user.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Notch-Technologies/wizy/cmd/server/database"
 	"github.com/Notch-Technologies/wizy/cmd/server/pb/user"
 	"github.com/Notch-Technologies/wizy/cmd/server/usecase"
+	"github.com/Notch-Technologies/wizy/types/key"
 )
 
 type UserServiceServer struct {
@@ -25,7 +26,17 @@ func NewUserServiceServer(
 
 func (uss *UserServiceServer) SetupKey(ctx context.Context, msg *user.SetupKeyMessage) (*user.SetupKeyMessage, error) {
 	sub := getSub(ctx)
-	fmt.Println(sub)
+	networkName := msg.GetNetworkID()
+	userGroupName := msg.GetUserGroupID()
+	job := msg.GetJob()
+	orgGroupID := msg.GetOrgGroupID()
+	permission := msg.GetPermission()
+
+	setupKey, err := uss.setupKeyUsecase.CreateSetupKey(networkName, userGroupName, job, orgGroupID, key.PermissionType(permission), sub)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(setupKey)
 
 	return &user.SetupKeyMessage{}, nil
 }
