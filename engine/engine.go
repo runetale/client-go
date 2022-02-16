c8aa512278c3fa8410183ce371a6dcf91f50d3d9
Shintarou Okumura <flekystyley@gmail.com>
2022-02-16 11:07:28 +0900

    update engine

--- engine/engine.go
+++ engine/engine.go
@@ -6,6 +6,7 @@ import (
 
 	grpc_client "github.com/Notch-Technologies/wizy/cmd/server/grpc_client"
 	"github.com/Notch-Technologies/wizy/cmd/server/pb/negotiation"
+	"github.com/Notch-Technologies/wizy/cmd/server/pb/peer"
 	"github.com/Notch-Technologies/wizy/wislog"
 )
 
@@ -33,12 +34,26 @@ func NewEngine(
 	}
 }
 
-func (e *Engine) Start() {
+// TODO:
+// 1. create Engine
+// 2. send stream message
+// 3. create management json
+// 4. return to stun and sturn
+// 5. send to stun and turn udp request
+// 6. send to signal offer
+// 7. connection peer to peer test
+// 8. management peers and how save the peer connectivity state? maybe sync mutex??
+func (e *Engine) Start(publicMachineKey string) {
+	e.syncMsgMux.Lock()
+	defer e.syncMsgMux.Unlock()
+
+	e.receiveClient(publicMachineKey)
+	e.syncClient(publicMachineKey)
 }
 
-func (e *Engine) ReceiveClient() {
+func (e *Engine) receiveClient(machineKey string) {
 	go func() {
-		err := e.client.Receive(e.stream, func(msg *negotiation.StreamMessage) error {
+		err := e.client.Receive(machineKey, func(msg *negotiation.StreamMessage) error {
 			e.syncMsgMux.Lock()
 			defer e.syncMsgMux.Unlock()
 
@@ -51,7 +66,27 @@ func (e *Engine) ReceiveClient() {
 			return
 		}
 	}()
-	e.client.WaitStreamConnected()
 
-	fmt.Println("connecting signal server")
+	e.client.WaitStreamConnected()
+}
+
+func (e *Engine) syncClient(machineKey string) {
+	go func() {
+		err := e.client.Sync(machineKey, func(update *peer.SyncResponse) error {
+			e.syncMsgMux.Lock()
+			defer e.syncMsgMux.Unlock()
+
+			fmt.Println("sync")
+			fmt.Println(update)
+
+			// TODO: (shintard) send signal offer
+
+			return nil
+		})
+		if err != nil {
+			fmt.Println("stopping recive management server")
+			return
+		}
+	}()
+	fmt.Println("connecting management server")
 }
