2c4d805cb875bfda99714deb863cfdbddca7ed4c
shinta <flekystyley@gmail.com>
2022-03-01 23:21:56 +0900

    added core client package

---
+++ core/client.go
@@ -0,0 +1,89 @@
+package core
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/Notch-Technologies/wizy/cmd/wissy/tun"
+	"github.com/Notch-Technologies/wizy/types/key"
+	"github.com/Notch-Technologies/wizy/utils"
+)
+
+type ClientCore struct {
+	WgPrivateKey   string
+	ServerHost     *url.URL
+	IgonoreTUNs    []string
+	TUNName        string
+	PreSharedKey   string
+	IfaceBlackList []string
+
+	path string
+}
+
+func NewClientCore(
+	path string, host string, port int,
+) (*ClientCore, error) {
+	scheme := host + ":" + strconv.Itoa(port)
+
+	h, err := url.Parse(scheme)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ClientCore{
+		ServerHost:  h,
+		IgonoreTUNs: []string{},
+		path:        path,
+	}, nil
+}
+
+func (c *ClientCore) writeClientCore(
+	path, wgPrivateKey, tunName string,
+	ifaceBlackList []string,
+) *ClientCore {
+	if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
+		log.Fatal(err)
+	}
+
+	c.WgPrivateKey = wgPrivateKey
+	c.TUNName = tunName
+	c.IfaceBlackList = ifaceBlackList
+
+	b, err := json.MarshalIndent(*c, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = utils.AtomicWriteFile(path, b, 0600); err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+func (c *ClientCore) GetClientCore() *ClientCore {
+	b, err := ioutil.ReadFile(c.path)
+	switch {
+	case errors.Is(err, os.ErrNotExist):
+		privKey, err := key.NewGenerateKey()
+		if err != nil {
+			panic(err)
+		}
+		return c.writeClientCore(c.path, privKey, tun.TunName(), []string{tun.TunName(), "tun0"})
+	case err != nil:
+		log.Fatal(err)
+		panic(err)
+	default:
+		var core ClientCore
+		if err := json.Unmarshal(b, &core); err != nil {
+			log.Fatalf("can not read client config file. %v", err)
+		}
+		return c.writeClientCore(c.path, core.WgPrivateKey, core.TUNName, core.IfaceBlackList)
+	}
+}
