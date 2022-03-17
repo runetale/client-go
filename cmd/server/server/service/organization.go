a4f400c378d84e8c4c97830b04d83562f2db8fa1
Shintarou Okumura <flekystyley@gmail.com>
2022-03-17 17:46:14 +0900

    added new organization service

--- cmd/server/server/service/organization.go
+++ cmd/server/server/service/organization.go
@@ -12,6 +12,7 @@ import (
 type OrganizationServerServiceCaller interface {
 	Create(ctx context.Context, req *organization.OrganizationCreateRequest) (*organization.OrganizationCreateResponse, error)
 	CreateAdminUser(ctx context.Context, req *organization.OrganizationCreateAdminUserRequest) (*organization.OrganizationCreateAdminUserResponse, error)
+	CreateNetwork(ctx context.Context, req *organization.OrganizationCreateNetworkRequest) (*organization.OrganizationCreateNetworkResponse, error)
 }
 
 type OrganizationServerService struct {
@@ -32,20 +33,20 @@ func (oss *OrganizationServerService) AuthFuncOverride(ctx context.Context, full
 	return ctx, nil
 }
 
+// TOOD: remove
 func (oss *OrganizationServerService) Create(ctx context.Context, req *organization.OrganizationCreateRequest) (*organization.OrganizationCreateResponse, error) {
 	organizationUsecase := usecase.NewOrganizationUsecase(oss.db, oss.auth0Client)
 
-	org, err := organizationUsecase.CreateOrganizationWithAuth0(req.GetName(), req.GetDisplayName())
+	org, err := organizationUsecase.CreateOrganizationWithAuth0(req.GetName())
 	if err != nil {
 		return nil, err
 	}
 
-	organizationGroup, err := organizationUsecase.CreateOrganization(req.GetName(), req.GetDisplayName(), org.ID)
+	organizationGroup, err := organizationUsecase.CreateOrganization(req.GetName(), org.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: Enable Connectionで
 	err = organizationUsecase.EnableOrganizationConnection(org.ID, true)
 	if err != nil {
 		return nil, err
@@ -56,6 +57,7 @@ func (oss *OrganizationServerService) Create(ctx context.Context, req *organizat
 	}, nil
 }
 
+// TOOD: remove
 func (oss *OrganizationServerService) CreateAdminUser(ctx context.Context, req *organization.OrganizationCreateAdminUserRequest) (*organization.OrganizationCreateAdminUserResponse, error) {
 	organizationUsecase := usecase.NewOrganizationUsecase(oss.db, oss.auth0Client)
 	user, err := organizationUsecase.CreateUser(req.GetEmail(), req.GetPassword(), oss.auth0Client.DatabaseConnectionName)
@@ -75,5 +77,59 @@ func (oss *OrganizationServerService) CreateAdminUser(ctx context.Context, req *
 
 	return &organization.OrganizationCreateAdminUserResponse{
 		OrganizationID: user.UserID,
+	}, nil
+}
+
+// 1. create organization account with auth0
+// 2. register organization information in database
+// 3. allow organization login methods
+// 4. register a user as a member of organization
+// 5. assign admin role
+//
+func (oss *OrganizationServerService) CreateNetwork(
+	ctx context.Context, req *organization.OrganizationCreateNetworkRequest,
+) (*organization.OrganizationCreateNetworkResponse, error) {
+	companyName := req.GetCompanyName()
+	userID := req.GetUserID()
+
+	organizationUsecase := usecase.NewOrganizationUsecase(oss.db, oss.auth0Client)
+	
+	// 1
+	//
+	auth0Org, err := organizationUsecase.CreateOrganizationWithAuth0(companyName)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2
+	//
+	org, err := organizationUsecase.CreateOrganization(companyName, auth0Org.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3
+	//
+	err = organizationUsecase.EnableOrganizationConnection(org.OrgID, true)
+	if err != nil {
+		return nil, err
+	}
+
+	// 4
+	//
+	err = organizationUsecase.AddMemberOnOrganization(userID, org.OrgID)
+	if err != nil {
+		return nil, err
+	}
+
+	// 5
+	//
+	err = organizationUsecase.AssignAdminRole(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &organization.OrganizationCreateNetworkResponse{
+		OrganizationID: userID,
 	}, nil
 }
