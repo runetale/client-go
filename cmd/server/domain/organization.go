123d469a246ff335de684cae6f1f5df88d9eb7c6
Shintarou Okumura <flekystyley@gmail.com>
2022-03-17 17:44:30 +0900

    remove display name

--- cmd/server/domain/organization.go
+++ cmd/server/domain/organization.go
@@ -7,16 +7,14 @@ import (
 type Organization struct {
 	ID          uint      `db:"id"`
 	Name        string    `db:"name"`
-	DisplayName string    `db:"display_name"`
 	OrgID       string    `db:"org_id"`
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
 }
 
-func NewOrganization(name, displayName, orgID string) *Organization {
+func NewOrganization(name, orgID string) *Organization {
 	return &Organization{
 		Name:        name,
-		DisplayName: displayName,
 		OrgID:       orgID,
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
