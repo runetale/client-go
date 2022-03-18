3343ed0b7181a7db6678ae3f862cb23e472a5bd9
shinta <flekystyley@gmail.com>
2022-03-18 17:51:11 +0900

    refactor migrations file

--- cmd/server/migrations/2_networks.up.sql
+++ cmd/server/migrations/2_networks.up.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS networks (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+  admin_network_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   ip VARCHAR(15) NOT NULL,
   cidr INT NOT NULL,
-  dns varchar(253) NOT NULL,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL
+
+  FOREIGN KEY (admin_network_id) REFERENCES admin_networks(id),
 );
 
 CREATE INDEX idx_networks_name ON networks(name);
--- cmd/server/migrations/5_user_groups.up.sql
+++ cmd/server/migrations/5_user_groups.up.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS user_groups (
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+  admin_network_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
-  permission INT NOT NULL,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL
+
+  FOREIGN KEY (admin_network_id) REFERENCES admin_networks(id),
 );
+
+CREATE INDEX idx_user_groups_name ON user_groups(name);
