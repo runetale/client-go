ecfc43ebd6e4fcf58b10bc1a8cfd8f474d01ca41
shinta <flekystyley@gmail.com>
2022-05-25 17:37:23 +0900

    update grpc client

--- client/grpc/helper.go
+++ client/grpc/helper.go
@@ -1,10 +1,13 @@
 package grpc
 
 import (
+	"fmt"
+
 	"google.golang.org/grpc/metadata"
 )
 
 func getLoginSessionID(md metadata.MD) string {
-	registered := md.Get("session")
+	registered := md.Get("session_id")
+	fmt.Println(registered)
 	return registered[0]
 }
--- client/grpc/server_client.go
+++ client/grpc/server_client.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Notch-Technologies/client-go/notch/dotshake/v1/login_session"
 	"github.com/Notch-Technologies/client-go/notch/dotshake/v1/machine"
+	"github.com/Notch-Technologies/dotshake/system"
 	"github.com/Notch-Technologies/dotshake/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
@@ -14,7 +15,7 @@ import (
 
 type ServerClientImpl interface {
 	GetMachine(mk string) (*machine.GetMachineResponse, error)
-	ConnectStreamPeerLoginSession(ctx context.Context, mk string) error
+	ConnectStreamPeerLoginSession(ctx context.Context, mk string) (*login_session.PeerLoginSessionResponse, error)
 }
 
 type ServerClient struct {
@@ -52,29 +53,40 @@ func (c *ServerClient) GetMachine(mk string) (*machine.GetMachineResponse, error
 	}, nil
 }
 
-func (c *ServerClient) ConnectStreamPeerLoginSession(ctx context.Context, mk string) error {
-	md := metadata.New(map[string]string{utils.MachineKey: mk})
+func (c *ServerClient) ConnectStreamPeerLoginSession(ctx context.Context, mk string) (*login_session.PeerLoginSessionResponse, error) {
+	var (
+		msg = &login_session.PeerLoginSessionResponse{}
+	)
+
+	sys := system.NewSysInfo()
+	md := metadata.New(map[string]string{utils.MachineKey: mk, utils.HostName: sys.Hostname, utils.OS: sys.OS})
 	newctx := metadata.NewOutgoingContext(ctx, md)
 
 	stream, err := c.loginSessionClient.StreamPeerLoginSession(newctx, grpc.WaitForReady(true))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	header, err := stream.Header()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	sessionid := getLoginSessionID(header)
-	fmt.Printf("sessionid: [%s]", sessionid)
+	fmt.Printf("sessionid: [%s]\n", sessionid)
 
 	for {
-		msg, err := stream.Recv()
+		msg, err = stream.Recv()
 		if err != nil {
-			return err
+			return nil, err
 		}
-		fmt.Println(msg)
-		return nil
+
+		err = stream.Send(&emptypb.Empty{})
+		if err != nil {
+			return nil, err
+		}
+		break
 	}
+
+	return msg, nil
 }
