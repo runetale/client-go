ddb1567b7cf756522e00d7fde688e504649e22b9
GitHub Action <action@github.com>
2024-02-08 07:56:08 +0000

    Sync to proto commit
    runetale/proto@f0ba2d2ef973e1399edeaff3cca7e778c4978a19

--- runetale/runetale/v1/oidc/oidc_grpc.pb.go
+++ runetale/runetale/v1/oidc/oidc_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,7 +20,8 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	OIDCService_Login_FullMethodName = "/protos.OIDCService/Login"
+	OIDCService_Login_FullMethodName        = "/protos.OIDCService/Login"
+	OIDCService_Authenticate_FullMethodName = "/protos.OIDCService/Authenticate"
 )
 
 // OIDCServiceClient is the client API for OIDCService service.
@@ -27,6 +29,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type OIDCServiceClient interface {
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
+	Authenticate(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type oIDCServiceClient struct {
@@ -46,11 +49,21 @@ func (c *oIDCServiceClient) Login(ctx context.Context, in *LoginRequest, opts ..
 	return out, nil
 }
 
+func (c *oIDCServiceClient) Authenticate(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, OIDCService_Authenticate_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // OIDCServiceServer is the server API for OIDCService service.
 // All implementations should embed UnimplementedOIDCServiceServer
 // for forward compatibility
 type OIDCServiceServer interface {
 	Login(context.Context, *LoginRequest) (*LoginResponse, error)
+	Authenticate(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
 }
 
 // UnimplementedOIDCServiceServer should be embedded to have forward compatible implementations.
@@ -59,6 +72,9 @@ type UnimplementedOIDCServiceServer struct {
 
 func (UnimplementedOIDCServiceServer) Login(context.Context, *LoginRequest) (*LoginResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (UnimplementedOIDCServiceServer) Authenticate(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Authenticate not implemented")
 }
 
 // UnsafeOIDCServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -90,6 +106,24 @@ func _OIDCService_Login_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _OIDCService_Authenticate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(OIDCServiceServer).Authenticate(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: OIDCService_Authenticate_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(OIDCServiceServer).Authenticate(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // OIDCService_ServiceDesc is the grpc.ServiceDesc for OIDCService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -100,6 +134,10 @@ var OIDCService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Login",
 			Handler:    _OIDCService_Login_Handler,
+		},
+		{
+			MethodName: "Authenticate",
+			Handler:    _OIDCService_Authenticate_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
